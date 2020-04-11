3bbcab784520c8571f3170a59255f6638ad328f5
p5yho <pogelsek.jernej@gmail.com>
2020-04-11 12:24:09 +0200

    fix

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
-const serveStatic = require("serve-static")
+const app = express();
 const path = require('path');
-app = express();
-app.use(serveStatic(path.join(__dirname, 'src')));
-const port = process.env.PORT || 80;
-app.listen(port);
+const port = 3000;
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + 'dist'));
+});
+
+app.listen(port, () => console.log(`url-shortener listening on port ${port}!`));
