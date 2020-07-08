d52785650e571acf1799ce36cbc67d5ae49ecff5
p5yho <pogelsek.jernej@gmail.com>
2020-07-08 20:08:57 +0200

    nov video

--- src/views/About.vue
+++ src/views/About.vue
@@ -48,7 +48,7 @@
         alt="Responsive image"
       />
         <video id="videoTutorial" width="65%" controls poster="../assets/Thumbnailll.png">
-          <source src="../assets/stranVideoNew.mp4" type="video/mp4">
+          <source src="../assets/stranVideonova.mp4" type="video/mp4">
           Your browser does not support the video tag.
         </video>
       <div id="gcode">
